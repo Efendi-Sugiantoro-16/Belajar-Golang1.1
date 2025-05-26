b994c1712627820fad1a4c181059075b4cb94632
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-05-26 19:56:55 +0700

    fixing codes

--- cjson/cjson.go
+++ cjson/cjson.go
@@ -2,7 +2,8 @@ package cjson
 
 import "github.com/gofiber/fiber/v2"
 
-// JSONResponse is a helper function to return JSON response with status code and data
+// JSONResponse adalah fungsi helper untuk membungkus respon JSON
+// Mengatur status HTTP dan mengembalikan data JSON kepada client
 func JSONResponse(c *fiber.Ctx, status int, data interface{}) error {
 	return c.Status(status).JSON(data)
 }
