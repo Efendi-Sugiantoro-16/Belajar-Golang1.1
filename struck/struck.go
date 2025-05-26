d81ee5cebf6a4d16a93074c2d116c7f16b7d153d
Efendi-Sugiantoro-16 <efendisugiantoro16@gmail.com>
2025-05-26 19:56:17 +0700

    fixing codes

--- struck/struck.go
+++ struck/struck.go
@@ -2,11 +2,11 @@ package struck
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// WarehouseItem represents an item in the warehouse inventory
+// WarehouseItem representasi pada item dalam warehouse inventory (Model)
 type WarehouseItem struct {
-    ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-    Name        string             `json:"name" bson:"name"`
-    Description string             `json:"description" bson:"description"`
-    Quantity    int                `json:"quantity" bson:"quantity"`
-    Location    string             `json:"location" bson:"location"`
+	ID          primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Description string             `json:"description" bson:"description"`
+	Quantity    int                `json:"quantity" bson:"quantity"`
+	Location    string             `json:"location" bson:"location"`
 }
